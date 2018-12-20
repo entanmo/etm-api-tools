65d5953d202d8a426d928c49b6e2eb40bb172cf8
SyAtlantis <1102960695@qq.com>
2018-12-20 18:11:58 +0800

    添加模块

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,42 +1,56 @@
 <template>
   <div class="home">
-    <div class="home-menu">
-      <a-menu mode="inline"
-              :openKeys="openKeys"
-              @openChange="onOpenChange"
-              style="width: 256px">
-        <a-sub-menu key="sub1">
-          <span slot="title">
-            <a-icon type="swap" /><span>交易</span></span>
-          <a-menu-item key="tr-1">转账</a-menu-item>
-          <a-menu-item key="tr-2">注册代理</a-menu-item>
-          <a-menu-item key="tr-3">锁仓</a-menu-item>
-          <a-menu-item key="tr-4">解锁仓</a-menu-item>
-          <a-menu-item key="tr-5">投票</a-menu-item>
-          <a-menu-item key="tr-6">取消投票</a-menu-item>
-        </a-sub-menu>
-        <a-sub-menu key="sub2">
-          <span slot="title">
-            <a-icon type="search" /><span>查询</span></span>
-          <a-menu-item key="si-1">账户信息</a-menu-item>
-          <a-menu-item key="si-2">交易信息</a-menu-item>
-          <a-menu-item key="si-3">区块信息</a-menu-item>
-        </a-sub-menu>
-        <a-sub-menu key="sub3">
-          <span slot="title">
-            <a-icon type="form" /><span>生成</span></span>
-          <a-menu-item key="cf-1">创世快</a-menu-item>
-          <a-menu-item key="cf-2">Option 10</a-menu-item>
-          <a-menu-item key="cf-3">Option 11</a-menu-item>
-          <a-menu-item key="cf-4">Option 12</a-menu-item>
-        </a-sub-menu>
-      </a-menu>
-    </div>
-    <div class="home-body">
-      <div v-show="openKeys[0] === 'sub1'"><Transactions /></div>
-      <div v-show="openKeys[0] === 'sub2'"><SeleteInfo /></div>
-      <div v-show="openKeys[0] === 'sub3'"><CreateFlie /></div>
-    </div>
+    <a-row>
+      <a-col :span="18"
+             :push="6">
+        <div class="home-menu">
+          <a-menu mode="inline"
+                  :openKeys="openKeys"
+                  @openChange="onOpenChange"
+                  style="width: 256px">
+            <a-sub-menu key="sub1">
+              <span slot="title">
+                <a-icon type="swap" /><span>交易</span></span>
+              <a-menu-item key="tr-1">转账</a-menu-item>
+              <a-menu-item key="tr-2">注册代理</a-menu-item>
+              <a-menu-item key="tr-3">锁仓</a-menu-item>
+              <a-menu-item key="tr-4">解锁仓</a-menu-item>
+              <a-menu-item key="tr-5">投票</a-menu-item>
+              <a-menu-item key="tr-6">取消投票</a-menu-item>
+            </a-sub-menu>
+            <a-sub-menu key="sub2">
+              <span slot="title">
+                <a-icon type="search" /><span>查询</span></span>
+              <a-menu-item key="si-1">账户信息</a-menu-item>
+              <a-menu-item key="si-2">交易信息</a-menu-item>
+              <a-menu-item key="si-3">区块信息</a-menu-item>
+            </a-sub-menu>
+            <a-sub-menu key="sub3">
+              <span slot="title">
+                <a-icon type="form" /><span>生成</span></span>
+              <a-menu-item key="cf-1">创世快</a-menu-item>
+              <a-menu-item key="cf-2">Option 10</a-menu-item>
+              <a-menu-item key="cf-3">Option 11</a-menu-item>
+              <a-menu-item key="cf-4">Option 12</a-menu-item>
+            </a-sub-menu>
+          </a-menu>
+        </div>
+      </a-col>
+      <a-col :span="6"
+             :pull="18">
+        <div class="home-body">
+          <div v-show="openKeys[0] === 'sub1'">
+            <Transactions />
+          </div>
+          <div v-show="openKeys[0] === 'sub2'">
+            <SeleteInfo />
+          </div>
+          <div v-show="openKeys[0] === 'sub3'">
+            <CreateFlie />
+          </div>
+        </div>
+      </a-col>
+    </a-row>
 
   </div>
 </template>
